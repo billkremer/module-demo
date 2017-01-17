19ab4b628c6312f01b747b984323a51232bbb06a
Bill Kremer <bkrems12@gmail.com>
2017-01-17 16:41:20 -0600

    removed console.log statements

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var modThree = require('./moduleThree');  // brings in node modules
   http.createServer(function ( req, res) {
       res.writeHead(200);  // 200 =ok response code 418 = im a teapot
       res.write(modThree.secondFunction());
-        console.log(modThree.secondFunction()); // prints to bash
-      res.write(modThree.firstFunction());      
-        console.log(modThree.firstFunction()); // prints to bash
+//        console.log(modThree.secondFunction()); // prints to bash
+      res.write(modThree.firstFunction());
+//        console.log(modThree.firstFunction()); // prints to bash
       res.end();  // ok to send response
 
   }).listen(3000);  // listening on port 3000
